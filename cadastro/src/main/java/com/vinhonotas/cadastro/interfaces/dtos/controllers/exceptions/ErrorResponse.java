31dc3d1e9426d6ca0db6b582794495c9b304b5d8
vanderleik <vanderleik@yahoo.com.br>
2024-01-18 19:03:22 -0300

    feat: VIN-52 - Criando CountryController

---
+++ cadastro/src/main/java/com/vinhonotas/cadastro/interfaces/dtos/controllers/exceptions/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.vinhonotas.cadastro.interfaces.dtos.controllers.exceptions;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class ErrorResponse {
+
+    private final String message;
+    private final int code;
+    private final String objectName;
+    private final List<ErrorObject> errors;
+}
