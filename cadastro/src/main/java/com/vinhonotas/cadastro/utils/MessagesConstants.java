2e127b18731fd9015b083b2e27b84810318d49bc
vanderleik <vanderleik@yahoo.com.br>
2024-01-18 08:49:20 -0300

    feat: VIN-48 - Criando StateService

---
+++ cadastro/src/main/java/com/vinhonotas/cadastro/utils/MessagesConstants.java
@@ -0,0 +1,10 @@
+package com.vinhonotas.cadastro.utils;
+
+public class MessagesConstants {
+    public static final String ERROR_WHEN_SAVING_STATE = "Erro ao gravar dados do estado";
+    public static final String STATE_NOT_FOUND = "Estado não encontrado";
+    public static final String STATE_NOT_FOUND_WITH_NAME = "Estado não encontrado com o nome: ";
+    public static final String STATE_NOT_FOUND_WITH_UF = "Estado não encontrado com a UF: ";
+    public static final String ERROR_UPDATE_STATE_DATA = "Erro ao atualizar dados do estado";
+    public static final String ERROR_DELETE_STATE_DATA = "Erro ao deletar estado";
+}
