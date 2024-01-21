c30e4b56f6ba69216d28ba431534d979b6c736e9
vanderleik <vanderleik@yahoo.com.br>
2024-01-21 19:29:28 -0300

    feat: criando VINHO

---
+++ vinho/src/test/java/com/vinhonotas/vinho/VinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinhonotas.vinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
