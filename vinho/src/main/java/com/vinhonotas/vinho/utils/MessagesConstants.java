0ff4a22f1e8e94517041e09dd4d1ec2f4d8333ae
vanderleik <vanderleik@yahoo.com.br>
2024-01-22 10:14:25 -0300

    feat: VIN-59 - Criando MessagesConstants

---
+++ vinho/src/main/java/com/vinhonotas/vinho/utils/MessagesConstants.java
@@ -0,0 +1,13 @@
+package com.vinhonotas.vinho.utils;
+
+public class MessagesConstants {
+
+    public static final String ERROR_CREATE_WINE = "Erro ao salvar vinho";
+
+    private MessagesConstants() {
+    }
+
+    public static MessagesConstants createMessagesConstants() {
+        return new MessagesConstants();
+    }
+}
