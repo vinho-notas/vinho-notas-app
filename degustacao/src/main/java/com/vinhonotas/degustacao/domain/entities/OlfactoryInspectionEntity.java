36f3e35b1722005259a8a5a04281cce84b028f7b
vanderleik <vanderleik@yahoo.com.br>
2024-01-29 15:53:19 -0300

    fix: VIN-146 - update OlfactoryInspectionEntity

---
+++ degustacao/src/main/java/com/vinhonotas/degustacao/domain/entities/OlfactoryInspectionEntity.java
@@ -0,0 +1,47 @@
+package com.vinhonotas.degustacao.domain.entities;
+
+import com.vinhonotas.degustacao.domain.enums.EnumClassificationType;
+import com.vinhonotas.degustacao.domain.enums.EnumIntensityType;
+import com.vinhonotas.degustacao.domain.enums.EnumPersistenceType;
+import com.vinhonotas.degustacao.domain.enums.EnumQualityType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
+@Table(name = "Tbolfactoryinspection", schema = "degustacao")
+public class OlfactoryInspectionEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, nullable = false, unique = true)
+    private UUID id;
+    @Column(name = "tastingdata")
+    private LocalDate tastingData;
+    @Column(name = "winetasted")
+    private String wineTasted;
+    @Column(name = "intensity")
+    @Enumerated(EnumType.STRING)
+    private EnumIntensityType intensity;
+    @Column(name = "persistence")
+    @Enumerated(EnumType.STRING)
+    private EnumPersistenceType persistence;
+    @Column(name = "quality")
+    @Enumerated(EnumType.STRING)
+    private EnumQualityType quality;
+    @Column(name = "aromas")
+    @Enumerated(EnumType.STRING)
+    private String aromas;
+    @Column(name = "classification")
+    @Enumerated(EnumType.STRING)
+    private EnumClassificationType classification;
+}
