fdd9a45d9c64d3309cdc42e4057ae43aa3fed6c2
vanderleik <vanderleik@yahoo.com.br>
2024-01-29 14:21:16 -0300

    feat: VIN-130 - criando EnumClassificationType

---
+++ degustacao/src/main/java/com/vinhonotas/degustacao/domain/enums/EnumClassificationType.java
@@ -0,0 +1,20 @@
+package com.vinhonotas.degustacao.domain.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * EnumClassificationType
+ * Refere-se a Classificação
+ */
+@Getter
+@RequiredArgsConstructor
+public enum EnumClassificationType {
+
+    LITTLE("Baixa"),
+    MEDIUM("Média"),
+    GOOD("Boa"),
+    EXCELLENT("Excelente");
+
+    private final String description;
+}
