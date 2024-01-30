24908ce086c618477ee05eb65fc15bd8e2511996
vanderleik <vanderleik@yahoo.com.br>
2024-01-30 11:02:26 -0300

    fix: VIN-132 - refatorando as entidades em função do mapeamento correto

--- degustacao/src/main/java/com/vinhonotas/degustacao/domain/entities/AromasEntity.java
+++ degustacao/src/main/java/com/vinhonotas/degustacao/domain/entities/AromasEntity.java
@@ -22,46 +22,61 @@ public class AromasEntity {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", updatable = false, nullable = false, unique = true)
     private UUID id;
+
     @Column(name = "tastingdata")
     private LocalDate tastingData;
+
     @Column(name = "winetasted")
     private String wineTasted;
+
     @Column(name = "fruity")
     @Enumerated(EnumType.STRING)
     private EnumFruityType fruity;
+
     @Column(name = "dryfruits")
     @Enumerated(EnumType.STRING)
     private EnumDryFruitsType dryFruits;
+
     @Column(name = "florals")
     @Enumerated(EnumType.STRING)
     private EnumFloralsType florals;
+
     @Column(name = "vegetablesandherbs")
     @Enumerated(EnumType.STRING)
     private EnumVegetablesAndHerbsType vegetablesAndHerbs;
+
     @Column(name = "minerals")
     @Enumerated(EnumType.STRING)
     private EnumMineralsType minerals;
+
     @Column(name = "spices")
     @Enumerated(EnumType.STRING)
     private EnumSpicesType spices;
+
     @Column(name = "animals")
     @Enumerated(EnumType.STRING)
     private EnumAnimalsType animals;
+
     @Column(name = "empireumatics")
     @Enumerated(EnumType.STRING)
     private EnumEmpireumaticsType empireumatics;
+
     @Column(name = "wood")
     @Enumerated(EnumType.STRING)
     private EnumWoodType wood;
+
     @Column(name = "chemicals")
     @Enumerated(EnumType.STRING)
     private EnumChemicalsAndEtherealType chemicals;
+
     @Column(name = "lacteal")
     @Enumerated(EnumType.STRING)
     private EnumLactealType lacteal;
+
     @Column(name = "sweets")
     @Enumerated(EnumType.STRING)
     private EnumSweetsType sweets;
+
     @Column(name = "classification")
     @Enumerated(EnumType.STRING)
     private EnumClassificationType classification;
--- degustacao/src/main/java/com/vinhonotas/degustacao/domain/entities/OlfactoryInspectionEntity.java
+++ degustacao/src/main/java/com/vinhonotas/degustacao/domain/entities/OlfactoryInspectionEntity.java
@@ -25,28 +25,35 @@ public class OlfactoryInspectionEntity {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", updatable = false, nullable = false, unique = true)
     private UUID id;
+
     @Column(name = "tastingdata")
     private LocalDate tastingData;
+
     @Column(name = "winetasted")
     private String wineTasted;
+
     @Column(name = "intensity")
     @Enumerated(EnumType.STRING)
     private EnumIntensityType intensity;
+
     @Column(name = "persistence")
     @Enumerated(EnumType.STRING)
     private EnumPersistenceType persistence;
+
     @Column(name = "quality")
     @Enumerated(EnumType.STRING)
     private EnumQualityType quality;
+
     @OneToOne
     @JoinColumn(name = "aromas_id", nullable = false)
-    @Column(name = "aromas")
     private AromasEntity aromas;
+
     @Column(name = "classification")
     @Enumerated(EnumType.STRING)
     private EnumClassificationType classification;
-    @Column(name = "tastingcard")
+
     @OneToOne(mappedBy = "olfactoryInspection")
+    @JoinColumn(name = "tastingcard_id", nullable = false)
     private TastingCardEntity tastingCard;
 
 }
