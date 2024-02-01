b03bba151fd0472f101ac4d111e071d6224c4daf
vanderleik <vanderleik@yahoo.com.br>
2024-02-01 16:59:25 -0300

    feat: VIN-143 - criando OlfactoryInspectionService

--- degustacao/src/main/java/com/vinhonotas/degustacao/utils/MessagesConstants.java
+++ degustacao/src/main/java/com/vinhonotas/degustacao/utils/MessagesConstants.java
@@ -2,10 +2,16 @@ package com.vinhonotas.degustacao.utils;
 
 public class MessagesConstants {
 
-    //AROMAS
+    //Aromas
     public static final String ERROR_WHEN_SAVING_AROMAS = "Erro ao salvar os aromas";
     public static final String AROMAS_NOT_FOUND = "Nenhum aroma encontrado";
     public static final String ERROR_WHEN_UPDATING_AROMAS = "Erro ao atualizar os aromas";
     public static final String ERROR_WHEN_DELETING_AROMAS = "Erro ao deletar os aromas";
+
+    //OlfactoryInspection
+    public static final String ERROR_WHEN_SAVING_OLFACTORY_INSPECTION = "Erro ao salvar a inspeção olfativa";
+    public static final String OLFACTORY_INSPECTION_NOT_FOUND = "Nenhuma inspeção olfativa encontrada";
+    public static final String ERROR_WHEN_UPDATING_OLFACTORY_INSPECTION = "Erro ao atualizar a inspeção olfativa";
+    public static final String ERROR_WHEN_DELETING_OLFACTORY_INSPECTION = "Erro ao deletar a inspeção olfativa";
 
 }
