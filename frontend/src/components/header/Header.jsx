2b3db8eca5cc2e92955bdc9fb1185d75dcdeb04d
vanderleik <vanderleik@yahoo.com.br>
2024-02-13 10:45:11 -0300

    feat: adicionado cor escura à navbar

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <div>
-      <Navbar expand="lg" className="bg-body-tertiary">
+      <Navbar expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
         <Container>        
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
@@ -14,7 +14,7 @@ const Header = () => {
                 <Link className="me-auto p-1 d-inline-block" to="/">Home</Link>
                 <Link className="me-auto p-1 d-inline-block" to="/login">Login</Link>
                 <Link className="me-auto p-1 d-inline-block" to="/cadastro">Cadastro</Link>             
-                <Link className="me-auto p-1 d-inline-block" to="/vinho">vinho</Link>             
+                <Link className="me-auto p-1 d-inline-block" to="/vinho">Vinho</Link>             
             </Nav>
             </Navbar.Collapse>
         </Container>
