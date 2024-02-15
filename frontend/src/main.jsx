95858cd50ce70d4ad971340998b00cf20523ef78
vanderleik <vanderleik@yahoo.com.br>
2024-02-15 14:52:17 -0300

    feat: adicionando PrimeReact às dependências
    Adicionado o PrimeReact às dependências com o intuito de utilizá-lo em alguns componentes.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,19 @@ import App from './App.jsx'
 import './index.css'
 
 import { BrowserRouter } from 'react-router-dom';
+import { PrimeReactProvider } from 'primereact/api';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';  
+import 'primereact/resources/primereact.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import './flags.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+    <PrimeReactProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PrimeReactProvider>
+  </React.StrictMode>
 )
