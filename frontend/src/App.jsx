dbe18556d4f4bcfa06990dd098db79d3c4d9479e
vanderleik <vanderleik@yahoo.com.br>
2024-02-15 10:39:38 -0300

    feat: criando componente Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,19 +8,21 @@ import Registration from './components/registration/Registration';
 import Wine from './components/wine/Wine';
 import WineReview from './components/wine/WineReview';
 import SearchWine from './components/wine/SearchWine';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <>
-    <Header />       
+      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />       
-        <Route path="/login" element={<Login />} />   
-        <Route path="/registration" element={<Registration />} />   
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
         <Route path="/wine" element={<Wine />} />
         <Route path="/wine-review" element={<WineReview />} />
         <Route path="/search-wine" element={<SearchWine />} />
       </Routes>
+      <Footer />
     </>
   )
 }
