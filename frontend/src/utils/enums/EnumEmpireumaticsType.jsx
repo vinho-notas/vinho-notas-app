48991e38448fc63550f88b4a866cf7c446557051
vanderleik <vanderleik@yahoo.com.br>
2024-02-17 17:41:54 -0300

    feat: VIN-207 criando EnumEmpireumaticsType

---
+++ frontend/src/utils/enums/EnumEmpireumaticsType.jsx
@@ -0,0 +1,12 @@
+class EnumEmpireumaticsType {
+
+    static ROASTING = 'Torrefação';
+    static TOASTED = 'Tostado';
+    static SMOKED = 'Defumado';
+    static TOBACCO = 'Tabaco';
+    static COFEE = 'Café';
+    static CHOCOLATE = 'Chocolate';
+    static CARAMEL = 'Caramelo';
+}
+
+export default EnumEmpireumaticsType;
