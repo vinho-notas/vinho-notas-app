c109f8ff34a0307a9bc39b329585c71593dcc46d
vanderleik <vanderleik@yahoo.com.br>
2024-02-17 17:24:33 -0300

    feat: VIT-204 - criando EnumClarityType

---
+++ frontend/src/utils/enums/EnumClarityType.jsx
@@ -0,0 +1,9 @@
+class EnumClarityType {
+
+    static VERY_CLEAR = 'Muito límpido';
+    static CLEAR = 'Límpido';
+    static LITTLE_CLEAR = 'Pouco límpido';
+    static CLOUDY = 'Turvo';
+}
+
+export default EnumClarityType;
