6806960423f8d5679591bdedfd480149d68a39f3
vanderleik <vanderleik@yahoo.com.br>
2024-02-18 10:35:17 -0300

    feat: VIN-210 - criando EnumIntensityType

---
+++ frontend/src/utils/enums/EnumIntensityType.jsx
@@ -0,0 +1,9 @@
+class EnumIntensityType {
+    
+    static VERY_INTENSE = 'Muito intenso';
+    static INTENSE = 'Intenso';
+    static LITTLE_INTENSE = 'Pouco intenso';
+    static WEAK = 'Fraco';
+}
+
+export default EnumIntensityType;
