d702597b76e4b591492a621e1e7dd5fb163d5d55
vanderleik <vanderleik@yahoo.com.br>
2024-02-18 11:37:29 -0300

    feat: VIN-222-Criar-EnumTanninType

---
+++ frontend/src/utils/enums/EnumTanninType.jsx
@@ -0,0 +1,10 @@
+class EnumTanninType {
+
+    static VERY_TANNIC = 'Muito tânico';
+    static QDEQUATE = 'Adequado';
+    static LITTLE_TANIC = 'Pouco tânico';
+    static ABSENT = 'Ausente';
+
+}
+
+export default EnumTanninType;
