819aa143e3e875bf42c90372859dd2ead348f6cd
vanderleik <vanderleik@yahoo.com.br>
2024-02-18 11:21:10 -0300

    feat: VIN-217-Criar EnumRedColorType

---
+++ frontend/src/utils/enums/EnumRedColorType.jsx
@@ -0,0 +1,11 @@
+class EnumRedColorType {
+
+    static VIOLET = 'Violáceo';
+    static RUBY_VIOLET_REFLECTIONS = 'Rubi com reflexos violáceos';
+    static RUBY = 'Rubi';
+    static RUBY_ORANGE_REFLECTIONS = 'Rubi com reflexos alaranjados';
+    static ORANGE = 'Alaranjado';
+
+}
+
+export default EnumRedColorType;
