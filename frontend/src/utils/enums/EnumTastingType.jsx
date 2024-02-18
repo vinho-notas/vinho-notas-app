c8a7ed4da9a5669c2d05084bdf5aab32aa9fb8d3
vanderleik <vanderleik@yahoo.com.br>
2024-02-18 11:39:02 -0300

    VIN-223-Criar-EnumTastingType

---
+++ frontend/src/utils/enums/EnumTastingType.jsx
@@ -0,0 +1,12 @@
+class EnumTastingType {
+
+    static VERTICAL = 'Degustação Vertical';
+    static HORIZONTAL = 'Degustação Horizontal';
+    static THEMATIC = 'Degustação Temática';
+    static BLIND = 'Degustação às Cegas';
+    static PAIRING = 'Degustação de Harmonização';
+    static COMPARATIVE = 'Degustação Comparativa';
+
+}
+
+export default EnumTastingType;
