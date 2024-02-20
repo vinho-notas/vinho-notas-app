bba2d23abb7a3c0d6b3e4bf2e891764574742b6b
vanderleik <vanderleik@yahoo.com.br>
2024-02-20 17:19:52 -0300

    feat: VIN-231 - criada tela de inspeção visual

--- frontend/src/components/tasting/Tasting.jsx
+++ frontend/src/components/tasting/Tasting.jsx
@@ -65,35 +65,6 @@ const Tasting = () => {
                 </div>
             );
     }
-
-
-    // return (
-    //     <>
-    //         <div>
-    //             <h1>Tela de degustação</h1>
-    //             <p>Na tela inicial tem uma tabela contendo todas as degustações registradas</p>
-    //             <p>Na tabela tem as opções de atualizar e excluir</p>
-    //             <h3>Registrar degustação</h3>
-
-    //             <p>Selecione ou cadastre uma degustação:</p>
-    //             <div>
-    //                 <Form>
-    //                     <Stack direction="horizontal" gap={3} style={{ marginTop: 20 }}>
-    //                         <Form.Control
-    //                             className="me-auto"
-    //                             placeholder="Escreva o nome do vinho aqui..."
-    //                             required
-    //                         />
-    //                         <Button variant="primary" type="submit">Buscar</Button>
-    //                     </Stack>
-    //                 </Form>
-    //                 <hr />
-    //             </div>
-               
-            
-    //         </div>
-    //     </>
-    // )
 }
 
 export default Tasting
