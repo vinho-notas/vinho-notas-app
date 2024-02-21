f6421e615b50b9ac914d78f27b9f0b0a37dea3a2
vanderleik <vanderleik@yahoo.com.br>
2024-02-21 16:51:43 -0300

    feat: VIN-234 - criando tela de avaliação geral

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,10 +11,6 @@ import SearchWine from './components/wine/SearchWine';
 import Footer from './components/footer/Footer';
 import Tasting from './components/tasting/Tasting';
 import TastingCard from './components/tasting/TastingCard';
-import OlfactoryInspection from './components/tasting/forms/OlfactoryInspection';
-import Aromas from './components/tasting/forms/Aromas';
-import VisualInspection from './components/tasting/forms/VisualInspection';
-import GustatoryInspection from './components/tasting/forms/GustatoryInspection';
 
 function App() {
   return (
@@ -29,10 +25,7 @@ function App() {
         <Route path="/tasting" element={<Tasting />} />
         <Route path="/tasting-card" element={<TastingCard />} />
         <Route path="/search-wine" element={<SearchWine />} />
-        <Route path="/visual-inspection" element={<VisualInspection />} />
-        <Route path="/olfactory-inspection" element={<OlfactoryInspection />} />       
-        <Route path="/gustatory-inspection" element={<GustatoryInspection />} />       
-      </Routes>      
+      </Routes>
       <Footer />
     </>
   )
---
+++ frontend/src/components/tasting/forms/GeneralValuation.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import { Card, Form } from 'react-bootstrap'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col';
+
+import EnumPointScale from '../../../utils/enums/EnumPointScale';
+
+const GeneralValuation = () => {
+    const pointScale = Object.values(EnumPointScale);
+
+    const [error, setError] = useState(false);
+
+    const submitFormData = (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <Card style={{ marginTop: 50 }}>
+            <Card.Header as="h5">Avaliação geral</Card.Header>
+            <Card.Body>
+                <Form onSubmit={submitFormData}>
+                    <Row className="mb-3">
+                        <Form.Group as={Row} className="mb-3">
+                            <Form.Text>
+                                <h5><strong>Vinho: </strong>Vinho XYZ</h5>
+                                <hr></hr>
+                            </Form.Text>
+                        </Form.Group>
+                    </Row>
+
+                    <Row className="mb-3">
+                        <Form.Group as={Col} className='mb-3'>
+                            <Form.Label>Avaliação geral</Form.Label>
+                            <Form.Select
+                                style={{ border: error ? '2px solid red' : '' }}
+                                name='pointScale'
+                                defaultValue=''
+                            >
+                                <option value='' disabled>De forma geral, como você classifica o vinho?</option>
+                                {Object.values(EnumPointScale).map((type, index) => (
+                                    <option key={index} value={type}>{type}</option>
+                                ))}
+                            </Form.Select>
+                        </Form.Group>
+                    </Row>
+                </Form>
+            </Card.Body>
+
+        </Card>
+
+    )
+}
+
+export default GeneralValuation
