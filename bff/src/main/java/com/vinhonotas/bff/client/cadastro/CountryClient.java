734906b135752347aedd900c6222c6a7a04978e1
vanderleik <vanderleik@yahoo.com.br>
2024-02-24 11:12:34 -0300

    feat: VIN-177 - alterando CountryClient pra expor somente os enpoints necessários

--- bff/src/main/java/com/vinhonotas/bff/client/cadastro/CountryClient.java
+++ bff/src/main/java/com/vinhonotas/bff/client/cadastro/CountryClient.java
@@ -1,18 +1,14 @@
 package com.vinhonotas.bff.client.cadastro;
 
-import com.vinhonotas.bff.interfaces.dtos.inputs.CountryInputDTO;
 import com.vinhonotas.bff.interfaces.dtos.outputs.CountryOutputDTO;
-import jakarta.validation.Valid;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 @FeignClient(name = "country", url = "${cadastro-api.url}")
 public interface CountryClient {
-
-    @PostMapping
-    CountryOutputDTO createCountry(@Valid @RequestBody CountryInputDTO countryInputDTO);
 
     @GetMapping
     List<CountryOutputDTO> getAllCountries();
@@ -22,13 +18,5 @@ public interface CountryClient {
 
     @GetMapping("/name/{name}")
     CountryOutputDTO getCountryByName(@PathVariable("name") String name);
-
-    @GetMapping("/continent/{continent}")
-    List<CountryOutputDTO> getCountryByContinent(@PathVariable("continent") String continent);
-
-    CountryOutputDTO updateCountry(@PathVariable("id") String id, @Valid @RequestBody CountryInputDTO countryInputDTO);
-
-    @DeleteMapping("/{id}")
-    Void deleteCountry(@PathVariable("id") String id);
 
 }
