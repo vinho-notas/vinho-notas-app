01045c8b45076b7fcbb0f23178e033cccacc1b6f
vanderleik <vanderleik@yahoo.com.br>
2024-02-25 10:34:54 -0300

    feat: VIN-263 - criando PersonService

--- bff/src/main/java/com/vinhonotas/bff/utils/MessagesConstants.java
+++ bff/src/main/java/com/vinhonotas/bff/utils/MessagesConstants.java
@@ -8,6 +8,12 @@ public class MessagesConstants {
     //STATE
     public static final String STATES_NOT_FOUND = "Nenhum estado encontrado";
 
+    //PERSON
+    public static final String ERROR_WHEN_SAVING_PERSON = "Erro ao salvar pessoa";
+    public static final String PERSONS_NOT_FOUND = "Nenhuma pessoa encontrada";
+    public static final String ERROR_UPDATE_PERSON_DATA = "Erro ao atualizar dados da pessoa";
+    public static final String ERROR_WHEN_DELETING_PERSON = "Erro ao deletar pessoa";
+
     private MessagesConstants() {
     }
 
