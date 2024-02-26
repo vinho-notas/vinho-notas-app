cab7af217b204938890b12a38120efc092cb9ed2
vanderleik <vanderleik@yahoo.com.br>
2024-02-26 08:33:45 -0300

    feat: VIN-266 - criando AddressService

--- bff/src/main/java/com/vinhonotas/bff/client/cadastro/AddressClient.java
+++ bff/src/main/java/com/vinhonotas/bff/client/cadastro/AddressClient.java
@@ -1,19 +1,28 @@
 package com.vinhonotas.bff.client.cadastro;
 
+import com.vinhonotas.bff.interfaces.dtos.inputs.AddressInputDTO;
 import com.vinhonotas.bff.interfaces.dtos.outputs.AddressOutputDTO;
+import jakarta.validation.Valid;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @FeignClient(name = "address", url = "${cadastro-api.url}")
 public interface AddressClient {
 
+    @PostMapping("/address")
+    AddressOutputDTO createAddress(@Valid @RequestBody AddressInputDTO addressInputDTO);
     @GetMapping("/address")
     List<AddressOutputDTO> getAllAddress();
 
     @GetMapping("/address/{id}")
     AddressOutputDTO getAddressById(@PathVariable("id") String id);
+
+    @PutMapping("/{id}")
+    AddressOutputDTO updateAddress(@PathVariable("id") String id, @Valid @RequestBody AddressInputDTO addressInputDTO);
+
+    @DeleteMapping("/{id}")
+    void deleteAddress(@PathVariable("id") String id);
 
 }
