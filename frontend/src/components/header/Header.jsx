bf4eab6266d0a0e37c7ed2647d87c6e4769adb2b
vanderleik <vanderleik@yahoo.com.br>
2024-03-01 14:59:13 -0300

    feat: VIN-245 - adicionando as rotas de cadastro e atualizando o tipo de menu

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,22 +1,40 @@
-import { TabMenu } from 'primereact/tabmenu';
+import { Menubar } from 'primereact/menubar';
 
 const Header = () => {
-
   const items = [
     { label: 'Home', icon: 'pi pi-home', url: '/' },
     { label: 'Login', icon: 'pi pi-user', url: '/login' },
-    { label: 'Cadastro', icon: 'pi pi-user-plus', url: '/registration' },
-    { label: 'Vinho', icon: 'pi pi-folder-open', url: '/wine' },
-    { label: 'Avaliação de vinho', icon: 'pi pi-folder-open', url: '/wine-review' },
-    { label: 'Degustação', icon: 'pi pi-folder-open', url: '/tasting' },
-    { label: 'Ficha de degustação', icon: 'pi pi-folder-open', url: '/tasting-card' },
-    { label: 'Listar degustação', icon: 'pi pi-folder-open', url: '/wine-tasted-list' },
-    { label: 'Consultas', icon: 'pi pi-folder-open', url: '/search-wine' }
+    { label: 'Cadastro', icon: 'pi pi-user-plus', 
+      items: [
+        { label: 'Cadastrar', icon: 'pi pi-user-plus', url: '/registration' },
+        { label: 'Usuários', icon: 'pi pi-users', url: '/users' },
+        { label: 'Pessoas', icon: 'pi pi-users', url: '/persons' },
+        { label: 'Endereços', icon: 'pi pi-folder-open', url: '/address' },
+        { label: 'Estados', icon: 'pi pi-folder-open', url: '/states' },
+        { label: 'Países', icon: 'pi pi-folder-open', url: '/countries' }
+      ]
+  },
+    
+    { label: 'Vinho', icon: 'pi pi-folder-open',
+      items: [
+        {label: 'Cadastrar Vinho', icon: 'pi pi-folder-open', url: '/wine' },
+        { label: 'Avaliar vinho', icon: 'pi pi-folder-open', url: '/wine-review' },
+        { label: 'Consultas', icon: 'pi pi-folder-open', url: '/search-wine' }
+      ]    
+    },
+    {
+      label: 'Degustação', icon: 'pi pi-folder-open',
+      items: [
+        { label: 'Registrar degustação', icon: 'pi pi-folder-open', url: '/tasting' },
+        { label: 'Ficha de degustação', icon: 'pi pi-folder-open', url: '/tasting-card' },
+        { label: 'Listar degustação', icon: 'pi pi-folder-open', url: '/wine-tasted-list' }
+      ]
+    }
   ];
 
   return (
     <div >
-      <TabMenu model={items} />
+      <Menubar model={items} />
     </div>
   )
 }
