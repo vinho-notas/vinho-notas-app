c881ab751cba0daca18a7ab8fc7029ad6bda8c59
vanderleik <vanderleik@yahoo.com.br>
2024-03-01 14:55:23 -0300

    feat: VIN-245 - criando Services

---
+++ frontend/src/service/registration/StateService.jsx
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const API_URL = `${import.meta.env.VITE_API_CADASTRO_URL}/states`;
+
+export const getAllStates = () => {
+    return axios.get(API_URL);
+};
+
+export const getStateById = (id) => {
+    return axios.get(`${API_URL}/${id}`);
+};
+
+export const getStateByName = (stateName) => {
+    return axios.get(`${API_URL}/name/${stateName}`);
+};
+
+export const getStateByUf = (uf) => {
+    return axios.get(`${API_URL}/uf/${uf}`);
+};
