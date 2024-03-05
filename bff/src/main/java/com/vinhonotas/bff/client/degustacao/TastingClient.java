f7216cd1d6c66341442af2af323a6d2b1083eebf
vanderleik <vanderleik@yahoo.com.br>
2024-03-05 17:05:08 -0300

    FEAT: VIN-310 - criando TastingClient

---
+++ bff/src/main/java/com/vinhonotas/bff/client/degustacao/TastingClient.java
@@ -0,0 +1,30 @@
+package com.vinhonotas.bff.client.degustacao;
+
+import com.vinhonotas.bff.configuration.FeignConfig;
+import com.vinhonotas.bff.interfaces.dtos.inputs.degustacao.TastingInputDTO;
+import com.vinhonotas.bff.interfaces.dtos.outputs.degustacao.TastingOutputDTO;
+import jakarta.validation.Valid;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@FeignClient(name = "tasting", url = "$(degustacao-api.url)", configuration = FeignConfig.class)
+public interface TastingClient {
+
+    @PostMapping("/tasting")
+    TastingOutputDTO createTasting(@Valid @RequestBody TastingInputDTO tastingInputDTO);
+
+    @GetMapping("/tasting")
+    List<TastingOutputDTO> getAllTastings();
+
+    @GetMapping("/tasting/{id}")
+    TastingOutputDTO getTastingById(@PathVariable String id);
+
+    @PutMapping("/tasting/{id}")
+    TastingOutputDTO updateTasting(@PathVariable String id, @Valid @RequestBody TastingInputDTO tastingInputDTO);
+
+    @DeleteMapping("/tasting/{id}")
+    void deleteTasting(@PathVariable String id);
+
+}
