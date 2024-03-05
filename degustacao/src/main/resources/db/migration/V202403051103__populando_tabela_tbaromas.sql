a33ba9a991db80fbfc0c01d17d7168b394ae1d35
vanderleik <vanderleik@yahoo.com.br>
2024-03-05 15:06:47 -0300

    feat: VIN-292 - criando scripts para popular as tabelas de degustação no banco

---
+++ degustacao/src/main/resources/db/migration/V202403051103__populando_tabela_tbaromas.sql
@@ -0,0 +1,11 @@
+INSERT INTO degustacao.tbaromas
+(id, tastingdata, winetasted, fruity, dryfruits, florals, vegetablesandherbs, minerals, spices, animals, empireumatics, wood, chemicals, lacteal, sweets)
+VALUES('8d61552b-4dc7-40b7-a74f-1fca3d2c9cd5', '2024-03-05', 'Baron Guillot Classique Esprit de Vigneron 2022', 'RASPBERRY', 'BRUNETTE', 'CLOVE', 'FENNEL', 'EARTH', 'INDIAN_CLOVE', 'LEATHER', 'CARAMEL', 'SAWDUST', 'ACETONE', 'BUTTER', 'BULLET');
+
+INSERT INTO degustacao.tbaromas
+(id, tastingdata, winetasted, fruity, dryfruits, florals, vegetablesandherbs, minerals, spices, animals, empireumatics, wood, chemicals, lacteal, sweets)
+VALUES('be56c6a0-f917-439a-80d8-062a9b454c11', '2024-03-05', 'El Molino Malbec 2022', 'RASPBERRY', 'BRUNETTE', 'CLOVE', 'FENNEL', 'EARTH', 'INDIAN_CLOVE', 'LEATHER', 'CARAMEL', 'SAWDUST', 'ACETONE', 'BUTTER', 'BULLET');
+
+INSERT INTO degustacao.tbaromas
+(id, tastingdata, winetasted, fruity, dryfruits, florals, vegetablesandherbs, minerals, spices, animals, empireumatics, wood, chemicals, lacteal, sweets)
+VALUES('ba4877d3-93e5-448b-b797-97a47db7dbe2', '2024-03-05', 'Don Simon Seleccion Tempranillo', 'RASPBERRY', 'BRUNETTE', 'CLOVE', 'FENNEL', 'EARTH', 'INDIAN_CLOVE', 'LEATHER', 'CARAMEL', 'SAWDUST', 'ACETONE', 'BUTTER', 'BULLET');
---
+++ degustacao/src/main/resources/db/migration/V202403051140__populando_tabela_tbtasting.sql
@@ -0,0 +1,11 @@
+INSERT INTO degustacao.tbtasting
+(id, tastingdata, tastingtype)
+VALUES('7a8ad3a6-2603-4aa2-a8d1-0b9d332bc32a', '2024-03-05', 'COMPARATIVE');
+
+INSERT INTO degustacao.tbtasting
+(id, tastingdata, tastingtype)
+VALUES('3aea3cb0-bf11-49b4-a9f7-a4e07782d7f8', '2024-03-05', 'COMPARATIVE');
+
+INSERT INTO degustacao.tbtasting
+(id, tastingdata, tastingtype)
+VALUES('7301f747-9455-4f1a-b458-161b19b0dfcf', '2024-03-05', 'COMPARATIVE');
