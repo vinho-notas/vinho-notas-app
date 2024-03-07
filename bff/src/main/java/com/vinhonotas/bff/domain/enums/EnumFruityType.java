32ceb250b8559232ff4f3e4bbff8f67eb41df3b6
vanderleik <vanderleik@yahoo.com.br>
2024-03-07 09:15:23 -0300

    fix: VIN-326 - adicionando propriedades nos Enums

--- bff/src/main/java/com/vinhonotas/bff/domain/enums/EnumFruityType.java
+++ bff/src/main/java/com/vinhonotas/bff/domain/enums/EnumFruityType.java
@@ -1,4 +1,31 @@
 package com.vinhonotas.bff.domain.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum EnumFruityType {
+
+    PINEAPPLE("Abacaxi"),
+    BLACKBERRY("Amora"),
+    PLUM("Ameixa"),
+    BANANA("Banana"),
+    CASSIS("Cassis"),
+    CHERRY("Cereja"),
+    FIG("Figo"),
+    RASPBERRY("Framboesa"),
+    GOOSEBERRY("Groselha"),
+    KIWI("Kiwi"),
+    ORANGE("Laranja"),
+    LEMON("Limão"),
+    LITTER("Maçã"),
+    MANGO("Manga"),
+    PASSION_FRUIT("Maracujá"),
+    WATERMELON("Melancia"),
+    PEAR("Pera"),
+    PEACH("Pêssego");
+
+    private final String description;
+
 }
