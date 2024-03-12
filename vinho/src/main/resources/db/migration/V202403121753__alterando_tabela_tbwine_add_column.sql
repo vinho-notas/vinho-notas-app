8e0071475ad9ac7377c3212923384f0b03dbd7ac
vanderleik <vanderleik@yahoo.com.br>
2024-03-12 18:46:22 -0300

    Update V202403121753__alterando_tabela_tbwine_add_column.sql

--- vinho/src/main/resources/db/migration/V202403121753__alterando_tabela_tbwine_add_column.sql
+++ vinho/src/main/resources/db/migration/V202403121753__alterando_tabela_tbwine_add_column.sql
@@ -1,5 +1,5 @@
-alter table vinho.tbwine
-add column dthreg timestamp default CURRENT_TIMESTAMP not null,
-add column userreg varchar(30) null,
-add column dthalt timestamp null,
-add column useralt varchar(30) null;
+-- alter table vinho.tbwine
+-- add column dthreg timestamp default CURRENT_TIMESTAMP not null,
+-- add column userreg varchar(30) null,
+-- add column dthalt timestamp null,
+-- add column useralt varchar(30) null;
