177d35f7fffd8c6bfd1a20c1dc1c97bb07029457
vanderleik <vanderleik@yahoo.com.br>
2024-03-13 10:13:50 -0300

    feat: VIN-272- adicionando colunas na tabela tbcountry

--- cadastro/src/main/java/com/vinhonotas/cadastro/interfaces/dtos/inputs/CountryInputDTO.java
+++ cadastro/src/main/java/com/vinhonotas/cadastro/interfaces/dtos/inputs/CountryInputDTO.java
@@ -3,10 +3,17 @@ package com.vinhonotas.cadastro.interfaces.dtos.inputs;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @Builder
 public class CountryInputDTO {
 
     private String countryName;
     private String continentName;
+    private LocalDateTime dthreg;
+    private String userreg;
+    private LocalDateTime dthalt;
+    private String useralt;
+
 }
