6d59a2546e2d2365bc9dce8ce3e3c5564a04f0dc
vanderleik <vanderleik@yahoo.com.br>
2024-03-14 11:05:22 -0300

    fix: VIN-347 - retirando a configuração do CORS no vinho

--- vinho/src/main/java/com/vinhonotas/vinho/interfaces/controllers/WineController.java
+++ vinho/src/main/java/com/vinhonotas/vinho/interfaces/controllers/WineController.java
@@ -19,7 +19,6 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/v1/wines")
 @Tag(name = "Vinhos", description = "Operações relacionadas a vinhos")
-@CrossOrigin(origins = "http://localhost:5173", maxAge = 3600)
 public class WineController {
 
     private final WineService wineService;
