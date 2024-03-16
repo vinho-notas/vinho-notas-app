21a4b425bc973551169bcb87209d7b74ea5b6482
vanderleik <vanderleik@yahoo.com.br>
2024-03-16 10:08:30 -0300

    feat: VIN-351 - criando o updatePointScale

--- frontend/src/components/review/ListPointScaleComponent.jsx
+++ frontend/src/components/review/ListPointScaleComponent.jsx
@@ -1,12 +1,25 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { FilterMatchMode } from 'primereact/api';
 import { InputText } from 'primereact/inputtext';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
+import { Card } from 'primereact/card';
+import { Toolbar } from 'primereact/toolbar';
+import { Button } from 'primereact/button';
+import { Dialog } from 'primereact/dialog';
+import { Dropdown } from 'primereact/dropdown';
 import useListPointScaleComponentHook from '../../hooks/review/usePointScaleComponentHook';
+import { updatePointScale } from '../../service/review/PointScaleService';
+import EnumPointScale from '../../utils/enums/EnumPointScale';
 
 const ListPointScaleComponent = () => {
-    const { pointScales } = useListPointScaleComponentHook();
+
+    //hooks
+    const { pointScales, navigate, fetchPointScales } = useListPointScaleComponentHook();
+    const [selectedPointScales, setSelectedPointScales] = useState(null);
+    const [editingPointScales, setEditingPointScales] = useState(null);
+    const [visibleEditDialog, setVisibleEditDialog] = useState(false);
+    const dt = useRef(null);
     const [filters, setFilters] = useState({
         global: { value: null, matchMode: FilterMatchMode.CONTAINS },
         whatTasted: { value: null, matchMode: FilterMatchMode.CONTAINS },
@@ -18,13 +31,127 @@ const ListPointScaleComponent = () => {
         pointScale: { value: null, matchMode: FilterMatchMode.CONTAINS }
     });
 
-    const [loading, setLoading] = useState(true);
 
+    //constants
+    const pointScale = Object.values(EnumPointScale);
+    const columns = [
+        { field: 'whatTasted', header: 'O que foi degustado', sortable: true, filterField: 'whatTasted' },
+        { field: 'whenTasted', header: 'Quando foi degustado', sortable: true, filterField: 'whenTasted' },
+        { field: 'whatSaw', header: 'Aspectos visuais', sortable: true, filterField: 'whatSaw' },
+        { field: 'whatAromas', header: 'Aromas', sortable: true, filterField: 'whatAromas' },
+        { field: 'whatFlavors', header: 'Sabores', sortable: true, filterField: 'whatFlavors' },
+        { field: 'whatOpinion', header: 'Opinião', sortable: true, filterField: 'whatOpinion' },
+        { field: 'pointScale', header: 'Avaliação final', sortable: true, filterField: 'pointScale' }
+    ]
+    const [visibleColumns, setVisibleColumns] = useState([]);
+
+    const [loading, setLoading] = useState(true);
     const [globalFilterValue, setGlobalFilterValue] = useState('');
 
-    useEffect(() => {
-        setLoading(false);
-    }, []);
+    const exportCSV = () => {
+        dt.current.exportCSV();
+    };
+
+
+    //handlers    
+    const onEditClick = () => {
+        if (selectedPointScales && selectedPointScales.length === 1) {
+            setEditingPointScales(selectedPointScales[0]);
+            setVisibleEditDialog(true);
+        } else {
+            alert('Selecione um vinho para editar.');
+        }
+    };
+
+    const saveEditedPointScale = async () => {
+        try {
+            await updatePointScale(editingPointScales.id, editingPointScales);
+            setVisibleEditDialog(false);
+            await fetchPointScales();
+            navigate("/wine-review-list")
+        } catch (error) {
+            console.log(error);
+        }
+
+    };
+
+    const leftToolbarTemplate = () => {
+        return (
+            <>
+                <Dialog visible={visibleEditDialog} onHide={() => setVisibleEditDialog(false)} header="Editar avaliação" modal>
+                    <div className="p-fluid">
+                        <div className="p-field">
+                            <label htmlFor="name">O que foi degustado</label>
+                            <InputText id="name" value={editingPointScales?.whatTasted || ''} onChange={(e) => setEditingPointScales({ ...editingPointScales, whatTasted: e.target.value })} />
+                        </div>
+                    </div>
+                    <div className="p-fluid">
+                        <div className="p-field">
+                            <label htmlFor="name">Quando foi degustado</label>
+                            <InputText id="name" value={editingPointScales?.whenTasted || ''} onChange={(e) => setEditingPointScales({ ...editingPointScales, whenTasted: e.target.value })} />
+                        </div>
+                    </div>
+                    <div className="p-fluid">
+                        <div className="p-field">
+                            <label htmlFor="name">Aspectos visuais</label>
+                            <InputText id="name" value={editingPointScales?.whatSaw || ''} onChange={(e) => setEditingPointScales({ ...editingPointScales, whatSaw: e.target.value })} />
+                        </div>
+                    </div>
+                    <div className="p-fluid">
+                        <div className="p-field">
+                            <label htmlFor="name">Aromas</label>
+                            <InputText id="name" value={editingPointScales?.whatAromas || ''} onChange={(e) => setEditingPointScales({ ...editingPointScales, whatAromas: e.target.value })} />
+                        </div>
+                    </div>
+                    <div className="p-fluid">
+                        <div className="p-field">
+                            <label htmlFor="name">Sabores</label>
+                            <InputText id="name" value={editingPointScales?.whatFlavors || ''} onChange={(e) => setEditingPointScales({ ...editingPointScales, whatFlavors: e.target.value })} />
+                        </div>
+                    </div>
+                    <div className="p-fluid">
+                        <div className="p-field">
+                            <label htmlFor="name">Opinião</label>
+                            <InputText id="name" value={editingPointScales?.whatOpinion || ''} onChange={(e) => setEditingPointScales({ ...editingPointScales, whatOpinion: e.target.value })} />
+                        </div>
+                    </div>
+                    <div className="p-fluid">
+                        <div className="p-field">
+                            <label htmlFor="name">Avaliação final</label>
+                            <Dropdown id="pointScale" value={editingPointScales?.pointScale || ''} options={pointScale} onChange={(e) => setEditingPointScales({ ...editingPointScales, pointScale: e.target.value })} />                     
+                        </div>
+                    </div>
+                    <div className="flex justify-content-end gap-2 mt-4">
+                        <Button label="Cancelar" icon="pi pi-times" onClick={() => setVisibleEditDialog(false)} className="p-button-danger" />
+                        <Button label="Salvar" icon="pi pi-check" onClick={saveEditedPointScale} className="p-button-success" />
+                    </div>
+
+
+                </Dialog>
+                <div className="flex flex-wrap gap-2">
+                    <Button rounded label="Editar" icon="pi pi-pencil" severity="secondary" onClick={onEditClick} disabled={!selectedPointScales || selectedPointScales.length !== 1} raised />
+
+                </div>
+            </>
+        )
+    };
+
+    const rightToolbarTemplate = () => {
+        return <Button rounded label="CSV" icon="pi pi-upload" className="p-button-help" onClick={exportCSV} raised />;
+    };
+
+    const onSelectionChange = (e) => {
+        setSelectedPointScales(e.value);
+    };
+
+    const onSelectAllChange = (e) => {
+        const _selectedPointScales = e.checked ? pointScales.map(scale => scale) : null;
+        if (_selectedPointScales) {
+            setSelectedPointScales(_selectedPointScales);
+        } else {
+            setSelectedPointScales(null);
+        }
+    };
 
     const onGlobalFilterChange = (e) => {
         const value = e.target.value;
@@ -47,10 +174,17 @@ const ListPointScaleComponent = () => {
 
     const header = renderHeader();
 
+    //effects
+    useEffect(() => {
+        setLoading(false);
+        setVisibleColumns(columns);
+    }, []);
+
+    //Render
 
     return (
-        <div className='card'>
-            <h5><strong>Avaliações</strong></h5>
+        <Card style={{ marginTop: 10 }} title="Lista de avaliações">
+            <Toolbar className="mb-4" start={leftToolbarTemplate} end={rightToolbarTemplate}></Toolbar>
             <DataTable
                 value={pointScales}
                 paginator
@@ -60,55 +194,21 @@ const ListPointScaleComponent = () => {
                 filters={filters}
                 globalFilterFields={['whatTasted', 'whenTasted', 'whatSaw', 'whatAromas', 'whatFlavors', 'whatOpinion', 'pointScale']}
                 header={header}
+                showGridlines
                 tableStyle={{ minWidth: '50rem' }}
                 emptyMessage="Nenhuma avaliação encontrada."
+                selectionMode="multiple"
+                selection={selectedPointScales}
+                onSelectionChange={onSelectionChange}
+                onSelectAll={onSelectAllChange}
+                ref={dt}
             >
-
-                <Column
-                    field='whatTasted'
-                    header='O que foi degustado'
-                    sortable
-                    filterField='whatTasted'
-                ></Column>
-                <Column
-                    field='whenTasted'
-                    header='Quando foi degustado'
-                    sortable
-                    filterField='whenTasted'
-                ></Column>
-                <Column
-                    field='whatSaw'
-                    header='Aspectos visuais'
-                    sortable
-                    filterField='whatSaw'
-                ></Column>
-                <Column
-                    field='whatAromas'
-                    header='Aromas'
-                    sortable
-                    filterField='whatAromas'
-                ></Column>
-                <Column
-                    field='whatFlavors'
-                    header='Sabores'
-                    sortable
-                    filterField='whatFlavors'
-                ></Column>
-                <Column
-                    field='whatOpinion'
-                    header='Opinião'
-                    sortable
-                    filterField='whatOpinion'
-                ></Column>
-                <Column
-                    field='pointScale'
-                    header='Avaliação final'
-                    sortable
-                    filterField='pointScale'
-                ></Column>
-
+                <Column selectionMode="multiple" headerStyle={{ width: '3rem' }} />
+                {visibleColumns.map((col) => (
+                    <Column key={col.field} field={col.field} header={col.header} sortable filterField={col.field} />
+                ))}
             </DataTable>
-        </div>
+        </Card>
     )
 }
 
