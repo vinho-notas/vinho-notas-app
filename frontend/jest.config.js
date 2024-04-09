c4e01129d3d048105a9bdd39844e6fbe1c569252
vanderleik <vanderleik@yahoo.com.br>
2024-04-09 10:10:47 -0300

    feat: VIN-173 - configurando Jest

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
     testEnvironment: 'jest-environment-jsdom',
     setupFilesAfterEnv: ['<rootDir>/.jest/jest.setup.js'],
+    moduleNameMapper: {
+        '\\.(gif|tff|eot|svg|png)$': '<rootDir>/.jest/mocks/fileMock.js',
+        '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    },
 
 }
