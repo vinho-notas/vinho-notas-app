50358c15f5536bc50590e32a7b57417cb4f0782c
vanderleik <vanderleik@yahoo.com.br>
2024-04-12 13:23:23 -0300

    feat: VIN-430 - removendo exceptions desnecessários

--- degustacao/src/main/java/com/vinhonotas/degustacao/domain/entities/exceptions/GustatoryInspectionNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.vinhonotas.degustacao.domain.entities.exceptions;
-
-public class GustatoryInspectionNotFoundException extends RuntimeException {
-
-    public GustatoryInspectionNotFoundException(String message) {
-        super(message);
-    }
-
-}
