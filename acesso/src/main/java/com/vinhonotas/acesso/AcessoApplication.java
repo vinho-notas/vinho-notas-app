8beae66b8745810822a5e5a104ea137499bdfae1
vanderleik <vanderleik@yahoo.com.br>
2024-04-16 11:27:59 -0300

    feat: VIN-248 - definindo a estrutura do microsservico de login

---
+++ acesso/src/main/java/com/vinhonotas/acesso/AcessoApplication.java
@@ -0,0 +1,13 @@
+package com.vinhonotas.acesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcessoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcessoApplication.class, args);
+	}
+
+}
