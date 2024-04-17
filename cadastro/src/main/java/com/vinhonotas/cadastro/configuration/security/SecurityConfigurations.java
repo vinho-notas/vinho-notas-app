a3a22381012a7925fdce057908ff068dad5a76db
vanderleik <vanderleik@yahoo.com.br>
2024-04-17 09:34:45 -0300

    feat: VIN-448 - criando AuthenticationController

--- cadastro/src/main/java/com/vinhonotas/cadastro/configuration/security/SecurityConfigurations.java
+++ cadastro/src/main/java/com/vinhonotas/cadastro/configuration/security/SecurityConfigurations.java
@@ -2,9 +2,14 @@ package com.vinhonotas.cadastro.configuration.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,6 +21,22 @@ public class SecurityConfigurations {
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/persons").permitAll()
+                )
                 .build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 }
---
+++ cadastro/src/main/resources/db/migration/V202404170909__alterando_tamanho_do_campo_password.sql
@@ -0,0 +1,2 @@
+ALTER TABLE cadastro.tbsystemuser ALTER COLUMN password SET DATA TYPE VARCHAR(255);
+ALTER TABLE cadastro.tbsystemuser ALTER COLUMN password SET NOT NULL;
