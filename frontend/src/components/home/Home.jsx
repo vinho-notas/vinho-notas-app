f6fc0b2fc495e3151fc20d25de377b2e07852890
vanderleik <vanderleik@yahoo.com.br>
2024-04-18 10:15:01 -0300

    feat: VIN-193 - alterando a rota para a página incial

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,69 +1,8 @@
-import { Button } from "primereact/button";
-import { useState } from 'react';
-import { InputText } from 'primereact/inputtext';
-import { Dialog } from 'primereact/dialog';
-import { login } from '../../service/registration/UserService';
-
 const Home = () => {
-  const [visible, setVisible] = useState(false);
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const processLogin = async () => {
-    const auth = {
-      email: username,
-      password: password
-    };
-
-    try {
-      const response = await login(auth);
-      console.log(response.data);
-      setVisible(false);
-    } catch (error) {
-      console.log(error);
-    }
-
-  };
-
-  return (
-    <>
-      <div className="jumbotron">
-        <h1 className="display-4">Vinho Notas</h1>
-        <h3>Bem-vindo à nossa plataforma de degustação de vinhos!</h3>
-        <p className="lead">Aqui, você terá a oportunidade de explorar, registrar e compartilhar suas experiências de degustação de vinhos de forma simples e intuitiva. Prepare-se para mergulhar em um universo de aromas, sabores e experiências sensoriais únicas. Estamos ansiosos para ajudá-lo em suas descobertas e tornar a sua experiência o mais memorável e prazerosa possível.</p>
-        <p className="lead"><em>Cheers! 🍷</em></p>
-        <p className="lead">A equipe do Vinho Notas</p>
-        <hr className="my-4"></hr>
-        <Button label="LOGIN" icon="pi pi-user" onClick={() => setVisible(true)} text size="large" />
-      </div>
-
-      <div>
-        <Dialog
-          visible={visible}
-          modal
-          onHide={() => setVisible(false)}
-          content={({ hide }) => (
-            <div className="flex flex-column px-8 py-5 gap-4" style={{ borderRadius: '12px', backgroundImage: 'radial-gradient(circle at left top, var(--primary-400), var(--primary-700))' }}>
-              <div className="inline-flex flex-column gap-2">
-                <label htmlFor="username" className="text-primary-50 font-semibold">E-mail</label>
-                <InputText id="username" label="Username" className="bg-white-alpha-20 border-none p-3 text-primary-50" value={username} onChange={(e) => setUsername(e.target.value)} />
-              </div>
-              <div className="inline-flex flex-column gap-2">
-                <label htmlFor="username" className="text-primary-50 font-semibold">Senha</label>
-                <InputText id="password" label="Password" className="bg-white-alpha-20 border-none p-3 text-primary-50" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-              </div>
-              <div className="flex align-items-center gap-2">
-                <Button label="Entrar" onClick={processLogin} text className="p-3 w-full text-primary-50 border-1 border-white-alpha-30 hover:bg-white-alpha-10"></Button>
-                <Button label="Cancelar" onClick={(e) => hide(e)} text className="p-3 w-full text-primary-50 border-1 border-white-alpha-30 hover:bg-white-alpha-10"></Button>
-              </div>
-              <div>
-                <p className="text-primary-50 text-center">Não possui uma conta? <a href="/registration" className="text-primary-100 hover:text-primary-50">Cadastre-se</a></p>
-              </div>
-            </div>
-          )}
-        ></Dialog>
-      </div>
-    </>
+  return (<>
+  <h1>Home Pge</h1>
+  </>
+    
   );
 };
 
