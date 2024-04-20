e78ff8206ce9cde8b043f4a4c33092e5d86ed0ac
vanderleik <vanderleik@yahoo.com.br>
2024-04-20 17:17:14 -0300

    bugfix - VIN-406 - corrigindo o cadastro

--- cadastro/src/main/java/com/vinhonotas/cadastro/application/services/AddressService.java
+++ cadastro/src/main/java/com/vinhonotas/cadastro/application/services/AddressService.java
@@ -2,6 +2,7 @@ package com.vinhonotas.cadastro.application.services;
 
 import com.vinhonotas.cadastro.domain.entities.AddressEntity;
 import com.vinhonotas.cadastro.interfaces.dtos.inputs.AddressInputDTO;
+import com.vinhonotas.cadastro.interfaces.dtos.inputs.EditAddressInputDTO;
 
 import java.util.List;
 import java.util.UUID;
@@ -11,7 +12,7 @@ public interface AddressService {
     AddressEntity create(AddressInputDTO addressInputDTO);
     List<AddressEntity> getAll();
     AddressEntity getById(UUID id);
-    AddressEntity update(UUID id, AddressInputDTO addressInputDTO);
+    AddressEntity update(UUID id, EditAddressInputDTO editAddressInputDTO);
     void delete(UUID id);
 
 }
