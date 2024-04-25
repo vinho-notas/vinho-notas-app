f9abf79fa19eba32248b06dc52a0484f2093e2ff
vanderleik <vanderleik@yahoo.com.br>
2024-04-25 09:49:26 -0300

    feat: VIN-454 - testes unitários para o componente Home.jsx

--- frontend/src/components/home/Home.test.jsx
+++ frontend/src/components/home/Home.test.jsx
@@ -3,18 +3,10 @@ import "@testing-library/jest-dom";
 import Home from './Home';
 
 describe('Home', () => {
-    it('renders correctly', () => {
+    it('should renders correctly', () => {
         render(<Home />);
         
-        expect(screen.getByText('Hello, world!')).toBeInTheDocument();
-        expect(screen.getByText('This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.')).toBeInTheDocument();
-        expect(screen.getByText('It uses utility classes for typography and spacing to space content out within the larger container.')).toBeInTheDocument();
-        expect(screen.getByText('Learn more')).toBeInTheDocument();
-    });
-
-    it('renders button correctly', () => {
-        render(<Home />);
-        expect(screen.getByText('Learn more').closest('a')).toHaveAttribute('href', '#');
+        expect(screen.getByText('Sejam bem-vindos ao Vinho Notas!')).toBeInTheDocument();        
     });
 
 });
