85927dbdc43b461f7f42238915e7e2e730d17d92
vanderleik <vanderleik@yahoo.com.br>
2024-04-25 09:46:57 -0300

    feat: VIN-454 - testes unitários para o componente Header.jsx

--- frontend/src/components/header/Header.test.jsx
+++ frontend/src/components/header/Header.test.jsx
@@ -4,51 +4,48 @@ import "@testing-library/jest-dom";
 
 describe('Header', () => {
 
-  it('renders correctly', () => {
+  it('should renders correctly', () => {
     render(<Header />);
 
     expect(screen.getByText('Home')).toBeInTheDocument();
-    expect(screen.getByText('Login')).toBeInTheDocument();
     expect(screen.getByText('Cadastro')).toBeInTheDocument();
     expect(screen.getByText('Vinho')).toBeInTheDocument();
     expect(screen.getByText('Degustação')).toBeInTheDocument();
+    expect(screen.getByText('Harmonização')).toBeInTheDocument();
   });
 
-  it('renders sub-menu items correctly', () => {
+  it('should renders sub-menu items correctly', () => {
     const { getByText } = render(<Header />);
 
     expect(getByText('Usuários')).toBeInTheDocument();
     expect(getByText('Pessoas')).toBeInTheDocument();
     expect(getByText('Endereços')).toBeInTheDocument();
     expect(getByText('Vinhos')).toBeInTheDocument();
-    expect(getByText('Avaliações')).toBeInTheDocument();
-    expect(getByText('Registrar degustação')).toBeInTheDocument();
-    expect(getByText('Ficha de degustação')).toBeInTheDocument();
-    expect(getByText('Listar fichas de degustação')).toBeInTheDocument();
+    expect(getByText('Avaliações')).toBeInTheDocument();    
+    expect(getByText('Fichas de degustação')).toBeInTheDocument();    
   });
 
-  it('renders menu icons correctly', () => {
+  it('should renders menu icons correctly', () => {
     const { container } = render(<Header />);
 
     expect(container.querySelector('.pi.pi-home')).toBeInTheDocument();
-    expect(container.querySelector('.pi.pi-user')).toBeInTheDocument();
+    expect(container.querySelector('.pi.pi-users')).toBeInTheDocument();
     expect(container.querySelector('.pi.pi-user-plus')).toBeInTheDocument();
     expect(container.querySelector('.pi.pi-folder-open')).toBeInTheDocument();
   });
 
-  it('renders menu urls correctly', () => {
+  it('should renders menu urls correctly', () => {
     const { getByText } = render(<Header />);
    
-    expect(getByText('Home').closest('a')).toHaveAttribute('href', '/');
-    expect(getByText('Login').closest('a')).toHaveAttribute('href', '/login');
+    expect(getByText('Home').closest('a')).toHaveAttribute('href', '/home');    
     expect(getByText('Usuários').closest('a')).toHaveAttribute('href', '/users');
     expect(getByText('Pessoas').closest('a')).toHaveAttribute('href', '/persons');
     expect(getByText('Endereços').closest('a')).toHaveAttribute('href', '/address');
     expect(getByText('Vinhos').closest('a')).toHaveAttribute('href', '/wine-list');
     expect(getByText('Avaliações').closest('a')).toHaveAttribute('href', '/wine-review-list');
-    expect(getByText('Registrar degustação').closest('a')).toHaveAttribute('href', '/tasting');
-    expect(getByText('Ficha de degustação').closest('a')).toHaveAttribute('href', '/tasting-card');
-    expect(getByText('Listar fichas de degustação').closest('a')).toHaveAttribute('href', '/tasting-list');
+    expect(getByText('Fichas de degustação').closest('a')).toHaveAttribute('href', '/tasting-list');
+    expect(getByText('Harmonização').closest('a')).toHaveAttribute('href', '/pairing-list');
+    
   });
 
 });
