716a932add1318cf2d133555d37a0d290b8facb2
vanderleik <vanderleik@yahoo.com.br>
2024-04-26 09:02:41 -0300

    feat: VIN-457 - adicionando Toast na tela de Endereços

--- bff/src/main/java/com/vinhonotas/bff/utils/MessagesConstants.java
+++ bff/src/main/java/com/vinhonotas/bff/utils/MessagesConstants.java
@@ -5,7 +5,7 @@ public class MessagesConstants {
     public static final String INVALID_FIELDS = "Campos inválidos";
     public static final String NOT_FOUND = "Nenhum registro encontrado";
     public static final String ERROR_WHEN_DELETING = "Erro ao deletar registro";
-    public static final String ERROR_WHEN_UPDATING = "Erro ao atualizar registro";
+    public static final String ERROR_WHEN_UPDATING = "Erro ao atualizar registro, verifique os campos informados";
     public static final String ERROR_WHEN_SAVING = "Erro ao salvar registro";
     public static final String BAD_REQUEST = "Requisição inválida, verifique os campos informados";
     public static final String ERROR_WHEN_LOGIN = "Usuário ou senha inválido, verifique.";
