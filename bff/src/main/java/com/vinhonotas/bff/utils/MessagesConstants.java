62b77fda7b28608c05d493a325cf206f1eb481fa
vanderleik <vanderleik@yahoo.com.br>
2024-04-26 08:22:11 -0300

    feat: VIN-457 - adicionando Toast na tela de login

--- bff/src/main/java/com/vinhonotas/bff/utils/MessagesConstants.java
+++ bff/src/main/java/com/vinhonotas/bff/utils/MessagesConstants.java
@@ -8,7 +8,7 @@ public class MessagesConstants {
     public static final String ERROR_WHEN_UPDATING = "Erro ao atualizar registro";
     public static final String ERROR_WHEN_SAVING = "Erro ao salvar registro";
     public static final String BAD_REQUEST = "Requisição inválida, verifique os campos informados";
-    public static final String ERROR_WHEN_LOGIN = "Erro ao realizar login";
+    public static final String ERROR_WHEN_LOGIN = "Usuário ou senha inválido, verifique.";
     public static final String RATE_LIMITER_MESSAGE = "Limite de requisições excedido. Tente novamente mais tarde.";
 
     private MessagesConstants() {
