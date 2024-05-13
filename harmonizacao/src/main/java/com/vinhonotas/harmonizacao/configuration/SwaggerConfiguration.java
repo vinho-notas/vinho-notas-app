c570b896860542bdeb1c3029382309c9768333d4
vanderleik <vanderleik@yahoo.com.br>
2024-05-13 11:22:28 -0300

    feat: VIN-484 - adicionando Swagger na API de harmonização

---
+++ harmonizacao/src/main/java/com/vinhonotas/harmonizacao/configuration/SwaggerConfiguration.java
@@ -0,0 +1,34 @@
+package com.vinhonotas.harmonizacao.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public OpenAPI openApiInformation() {
+        Server localServer = new Server()
+                .url("http://localhost:8085")
+                .description("Localhost Server URL");
+        Contact contact = new Contact()
+                .email("vanderlei.master@gmail.com")
+                .name("Vanderlei Kleinschmidt");
+        Info info = new Info()
+                .contact(contact)
+                .description("Api de harmonização de vinhos")
+                .title("Harmonização de Vinhos")
+                .version("V0.0.1")
+                .license(new License()
+                        .name("Apache 2.0")
+                        .url("http://springdoc.org"));
+        return new OpenAPI()
+                .info(info)
+                .addServersItem(localServer);
+    }
+}
