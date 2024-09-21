19e6a708aa97bba73a90d7ecfa85e143ee557050
vanderleik <vanderleik@yahoo.com.br>
2024-09-21 19:15:49 -0300

    feat: VIN-495 - Update WineRepository

--- vinho/src/main/java/com/vinhonotas/vinho/application/gateways/WineRepository.java
+++ vinho/src/main/java/com/vinhonotas/vinho/application/gateways/WineRepository.java
@@ -1,9 +1,10 @@
 package com.vinhonotas.vinho.application.gateways;
 
 import com.vinhonotas.vinho.domain.entities.wine.WineDomain;
+import com.vinhonotas.vinho.infraestructure.controller.dtos.input.WineInputDTO;
 
 public interface WineRepository {
 
-    WineDomain createWine(WineDomain wineDomain);
+    WineDomain createWine(WineInputDTO wineInputDTO);
 
 }
