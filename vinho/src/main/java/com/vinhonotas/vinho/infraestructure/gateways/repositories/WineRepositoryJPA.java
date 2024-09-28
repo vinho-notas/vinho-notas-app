38c7ca49dd8827f6361fbaebaec41d2c4de31f78
vanderleik <vanderleik@yahoo.com.br>
2024-09-28 17:22:19 -0300

    feat: VIN-495 - Criando WineRepositoryJPA

---
+++ vinho/src/main/java/com/vinhonotas/vinho/infraestructure/gateways/repositories/WineRepositoryJPA.java
@@ -0,0 +1,25 @@
+package com.vinhonotas.vinho.infraestructure.gateways.repositories;
+
+import com.vinhonotas.vinho.application.gateways.CreateWineRepository;
+import com.vinhonotas.vinho.domain.entities.wine.WineDomain;
+import com.vinhonotas.vinho.infraestructure.gateways.entities.WineEntity;
+import com.vinhonotas.vinho.infraestructure.gateways.mappers.WineEntityMapper;
+import com.vinhonotas.vinho.infraestructure.persistence.WineRepository;
+
+public class WineRepositoryJPA implements CreateWineRepository {
+
+    private final WineRepository wineRepository;
+    private final WineEntityMapper wineEntityMapper;
+
+    public WineRepositoryJPA(WineRepository wineRepository, WineEntityMapper wineEntityMapper) {
+        this.wineRepository = wineRepository;
+        this.wineEntityMapper = wineEntityMapper;
+    }
+
+    @Override
+    public WineEntity createWine(WineDomain wineDomain) {
+        WineEntity wineEntity = wineEntityMapper.toWineEntity(wineDomain);
+        return wineRepository.save(wineEntity);
+    }
+
+}
