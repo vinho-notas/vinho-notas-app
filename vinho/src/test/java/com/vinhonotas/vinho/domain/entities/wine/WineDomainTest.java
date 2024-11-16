a8595d6e6276eb224dc16498544468901e40ebd5
vanderleik <vanderleik@yahoo.com.br>
2024-11-16 17:20:43 -0300

    feat: VIN-504 - update WineDomain

--- vinho/src/test/java/com/vinhonotas/vinho/domain/entities/wine/WineDomainTest.java
+++ vinho/src/test/java/com/vinhonotas/vinho/domain/entities/wine/WineDomainTest.java
@@ -2,13 +2,13 @@ package com.vinhonotas.vinho.domain.entities.wine;
 
 import com.vinhonotas.vinho.domain.enums.EnumWineClassification;
 import com.vinhonotas.vinho.domain.enums.EnumWineType;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class WineDomainTest {
 
@@ -130,6 +130,31 @@ class WineDomainTest {
                         .harmonization("Carnes vermelhas grelhadas")
                         .build())
                 .build());
+
+        Assertions.assertNotNull(wineDomain1);
+        Assertions.assertNotNull(wineDomain2);
+        Assertions.assertNotNull(wineDomain3);
+        Assertions.assertNotNull(wineDomain4);
+
+        assertEquals("Luca Bosio Vineyards Barolo DOCG 2020", wineDomain1.getName());
+        assertEquals("Luca Bosio Vineyards Barolo DOCG 2020", wineDomain2.getName());
+        assertEquals("Expedicion Single Vineyard Selection Sauvignon Blanc Central Valley D.O. 2023", wineDomain3.getName());
+        assertEquals("Expedicion Single Vineyard Selection Sauvignon Blanc Central Valley D.O. 2023", wineDomain4.getName());
+
+        assertNotNull(wineDomain1.getWineDetails());
+        assertNotNull(wineDomain2.getWineDetails());
+        assertNotNull(wineDomain3.getWineDetails());
+        assertNotNull(wineDomain4.getWineDetails());
+
+        assertNotNull(wineDomain1.getPurchaseInfo());
+        assertNotNull(wineDomain2.getPurchaseInfo());
+        assertNotNull(wineDomain3.getPurchaseInfo());
+        assertNotNull(wineDomain4.getPurchaseInfo());
+
+        assertNotNull(wineDomain1.getWineOrigin());
+        assertNotNull(wineDomain2.getWineOrigin());
+        assertNotNull(wineDomain3.getWineOrigin());
+        assertNotNull(wineDomain4.getWineOrigin());
 
         assertEquals("LuREDR2020It", wineDomain1.getSku());
         assertEquals("LuWHDE2023It", wineDomain2.getSku());
